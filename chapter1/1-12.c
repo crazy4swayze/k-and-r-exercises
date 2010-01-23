84feb8cb33c4402aab761c08aaac4a342095097f
Karl Thornton <eliza.fritzl@gmail.com>
2010-01-23 16:12:04 +0000

    chapter 1 progress

---
+++ chapter1/1-12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t' )
+            putchar('\n');
+        else 
+            putchar(c);
+    }
+
+    return 0;
+}
