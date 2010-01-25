23ab252ff78396493dbff985202ce49374d27491
Karl Thornton <eliza.fritzl@gmail.com>
2010-01-25 12:02:39 +0000

    chapter 1 progress

---
+++ chapter1/1-15.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* fahr to cel */
+void ftoc(int, int);
+
+int main(void) {
+    float fahr;
+    int upper, step;
+
+    upper = 300;
+    step  = 20;
+
+    ftoc(step, upper);
+
+    return 0;
+}
+
+void ftoc(int step, int upper) {
+    float fahr, celsius;
+
+    fahr = 0;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return;
+}
